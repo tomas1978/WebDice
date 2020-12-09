1f88012f712281dbdfc06af32bbbf24d7c6db672
tomas1978 <tomas.nilsson78@gmail.com>
2020-12-09 18:46:27 +0100

    Added two functions
    Added drafts for functions to simulate and write results to log

--- diceSimulation.js
+++ diceSimulation.js
@@ -1,5 +1,23 @@
-var simButton=document.getElementById("simulationButton");
-simButton.onClick="changeText()";
+//var simButton=document.getElementById("simulationButton");
+//simButton.onClick"changeText()";
+
+simulate(1000);
+writeResult();
+
+var results=[0,0,0,0,0,0];
+
+function simulate(simulations) {
+	for(var i=0;i<simulations;i++) {
+		let result=Math.floor(Math.random() * 6) + 1;
+		results[result-1]++;
+	}
+}
+
+function writeResult() {
+	for(var i=0;i<6;i++) {
+		Console.log(i+": "+results[i])
+	}
+}
 
 function changeText() {
 	var element = document.getElementById("test");
